48a4546d56def1a54825449eeb60763bd07d6651
nguyentrungkien11K <trungkien08033@gmail.com>
2025-02-19 18:54:44 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import sqlite3
 import bcrypt
 
 # Link ảnh nền trên GitHub (sau khi tải lên, thay đường dẫn đúng vào đây)
-BACKGROUND_IMAGE = "https://github.com/nguyentrungkien11K/trung-kien-/blob/main/pascal-debrunner-wvZ3zzUftD8-unsplash.jpg"
+BACKGROUND_IMAGE = "https://raw.githubusercontent.com/nguyentrungkien11K/trung-kien-/refs/heads/main/pascal-debrunner-wvZ3zzUftD8-unsplash.jpg"
 
 # CSS để tạo giao diện giống mẫu
 st.markdown(
